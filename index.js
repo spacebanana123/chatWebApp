55e3425c8b6737d089f0ebb9ac386641fd9c2fa2
Matt Peng <mattukepeng@gmail.com>
2023-10-01 10:21:02 -0500

    username

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var username = "user-";
+for (var i = 0; i < 3; i++) {
+    username += Math.floor(Math.random() * 10).toString();
+}
+username += '-';
+for (var i = 0; i < 5; i++) {
+    username += Math.floor(Math.random() * 10).toString();
+}
+
+document.getElementById('username').innerHTML = "Using username: " + username;
+
 function sanitize(string) {
     const map = {
         '&': '&amp;',
@@ -18,12 +29,36 @@ box.addEventListener("keydown", function (e) {
     }
 });
 
-
 function send(){
     var message = sanitize(document.getElementById('message').value)
     console.log(message)
     document.getElementById('message').value = ''
     if(message != ''){
-        document.getElementById('chatBox').innerHTML = document.getElementById('chatBox').innerHTML + '<br>' + message
+        document.getElementById('chatBox').innerHTML = document.getElementById('chatBox').innerHTML + '<br>' + username + ": " + message
+    }
+    sendServer();
+}
+
+const url = 'https://chat-server.generationdnd.tk';
+var roomId = ''
+function sendServer(){
+    const Http = new XMLHttpRequest();
+    Http.open("POST", url);
+    var params = 'username=' + username + '&message=' + sanitize(document.getElementById('message').value) + '&roomId=' + roomId;
+    Http.send(params);
+
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+    }
+}
+
+function openServer(){
+    const Http = new XMLHttpRequest();
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+        roomId = Http.responseText
     }
 }
